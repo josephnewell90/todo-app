701133e05e7c31b699feffcbb6ecb7f2b5a14017
josephnewell90 <josephnewell90@gmail.com>
2015-11-05 08:56:04 -0700

    Login refined, missing show password icon

--- src/scripts/controllers/login.js
+++ src/scripts/controllers/login.js
@@ -1,31 +1,37 @@
 angular
   .module('LoginController', [
     'characterSheet.auth',
-    'characterSheet.users'
+    'characterSheet.users',
+    'toggleDirective',
   ])
   .controller('LoginController', [
     'auth',
     'users',
-    function (auth) {
+    function (auth, users) {
 
-      this.inputType = 'signin';
+      var login = this;
 
-      this.submit = function(email, password)
+      login.inputType = 'signin';
+
+      login.submit = function(email, password)
       {
-        this[this.inputType](email, password)
+        login.errorMessage = null;
+
+        login[login.inputType](email, password)
           .then(function(res) {
             // TODO redirect to the character sheet selection page
           })
           .catch(function(res) {
             console.log(res.status, res.data);
+            login.errorMessage = res.data.message;
           });
       };
 
-      this.signin = function(email, password) {
+      login.signin = function(email, password) {
         return auth.login(email, password);
       };
 
-      this.register = function(email, password, confirmPassword) {
+      login.register = function(email, password, confirmPassword) {
         return users
           .create({
             email: email,
---
+++ src/scripts/directives/toggle.js
@@ -0,0 +1,22 @@
+angular
+  .module('toggleDirective', [])
+  .directive('passwordToggle', function() {
+    return {
+      replace: true,
+      restrict: 'AE', //Can be an attribute (A) or a tag name (E)
+      scope: {
+        value: '=',
+      },
+      template: [
+        '<button type="button" ng-click="value = !value" class="btn btn-default" ng-class="{active: value}">',
+        ' <span ng-if="value" class="glyphicon glyphicon-{{ active }}"></span>',
+        ' <span ng-if="!value" class="glyphicon glyphicon-{{ inactive }}"></span>',
+        '</button>',
+      ].join(''), //Stuff that will be going in here. You can also use templateURL and point it to a file with the template in it if it's going to be a long one
+      link: function(scope, elem, attrs) {
+        scope.active = attrs.active;
+        scope.inactive = attrs.inactive;
+        console.log(scope);
+      },
+    };
+  });
